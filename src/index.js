ef2c4cb6d1309e56f996031cba4122093472ded4
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2019-08-29 18:04:56 -0700

    Dynamically updating m3u8 and .ts files

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ class HlsjsIPFSLoader {
     } else {
       this.debug = config.debug
     }
+    if(config.m3u8provider) {
+      this.m3u8provider = config.m3u8provider;
+    } else {
+      this.m3u8provider = null;
+    }
+    if(config.tsListProvider) {
+      this.tsListProvider = config.tsListProvider;
+    } else {
+      this.tsListProvider = null;
+    }
   }
 
   destroy() {
@@ -27,6 +37,20 @@ class HlsjsIPFSLoader {
     this.retryDelay = config.retryDelay
     this.loadInternal()
   }
+  /**
+   * Call this by getting the HLSIPFSLoader instance from hls.js hls.coreComponents[0].loaders.manifest.setM3U8Provider()
+   * @param {function} provider 
+   */
+  setM3U8Provider(provider) {
+    this.m3u8provider = provider;
+  }
+  /**
+   * 
+   * @param {function} provider 
+   */
+  setTsListProvider(provider) {
+    this.setTsListProvider = provider;
+  }
 
   loadInternal() {
     const { stats, context, config, callbacks } = this
@@ -37,6 +61,37 @@ class HlsjsIPFSLoader {
     const urlParts = context.url.split("/")
     const filename = urlParts[urlParts.length - 1]
 
+    if(filename.split(".")[1] === "m3u8" && this.m3u8provider !== null) {
+      const res = this.m3u8provider();
+      let data;
+      if(Buffer.isBuffer(res)) {
+        data = buf2str(res)
+      } else {
+        data = res;
+      }
+      const response = { url: context.url, data: data }
+      callbacks.onSuccess(response, stats, context)
+      return;
+    }
+    if(filename.split(".")[1] === "m3u8" && this.tsListProvider !== null) {
+      var tslist = this.tsListProvider();
+      var hash = tslist[filename];
+      if(hash) {
+        this.cat(hash).then(res => {
+          let data;
+          if(Buffer.isBuffer(res)) {
+            data = buf2str(res)
+          } else {
+            data = res;
+          }
+          stats.loaded = stats.total = data.length
+          stats.tload = Math.max(stats.tfirst, performance.now())
+          const response = { url: context.url, data: data }
+          callbacks.onSuccess(response, stats, context)
+        });
+      }
+      return;
+    }
     getFile(this.ipfs, this.hash, filename, this.debug).then(res => {
       const data = (context.responseType === 'arraybuffer') ? res : buf2str(res)
       stats.loaded = stats.total = data.length
@@ -49,7 +104,12 @@ class HlsjsIPFSLoader {
 
 function getFile(ipfs, rootHash, filename, debug) {
   debug(`Fetching hash for '${rootHash}/${filename}'`)
-    
+  if(filename === null) {
+    return ipfs.cat(rootHash).then( value => {
+      debug(`Received data for file '${rootHash}' size: ${value.length}`)
+      return value
+    });
+  }
   return ipfs.ls(rootHash).then(res => {
     const link = res.find(({ name }) => (name === filename))
 
